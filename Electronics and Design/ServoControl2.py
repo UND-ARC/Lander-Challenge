3034ac133472a1463d03b067b873858674e81298
Welterman <unknown24263@gmail.com>
2025-02-27 19:27:17 -0600

    Updated to make gimbal angles accurate

--- Electronics and Design/ServoControl2.py
+++ Electronics and Design/ServoControl2.py
@@ -1,4 +1,4 @@
-import pigpio
+#import pigpio
 import math
 import time
 
@@ -8,30 +8,30 @@ h_u = 4.160360
 r_l = 0.47
 h_l = 2.302372
 
-pi = pigpio.pi()
-pi.set_mode(servo_pin, pigpio.OUTPUT)
+#pi = pigpio.pi()
+#pi.set_mode(servo_pin, pigpio.OUTPUT)
 
 def angle_upper(angle):
-    phi_rad_u = math.asin(r_u*math.sin(angle*pi/180)/math.sqrt(r_u**2 + h_u**2 - 2*r_u*h_u*math.cos(angle*pi/180)))
-    phi_deg_u = phi_rad_u * 180/pi
+    phi_rad_u = math.asin(r_u*math.sin(angle*math.pi/180)/math.sqrt(r_u**2 + h_u**2 - 2*r_u*h_u*math.cos(angle*math.pi/180)))
+    phi_deg_u = phi_rad_u * 180/math.pi
     return phi_deg_u
 
 def angle_lower(angle):
-    phi_rad_l = math.asin(r_l*math.sin(angle*pi/180)/math.sqrt(r_l**2 + h_l**2 - 2*r_l*h_l*math.cos(angle*pi/180)))
-    phi_deg_l = phi_rad_l * 180/pi
+    phi_rad_l = math.asin(r_l*math.sin(angle*math.pi/180)/math.sqrt(r_l**2 + h_l**2 - 2*r_l*h_l*math.cos(angle*math.pi/180)))
+    phi_deg_l = phi_rad_l * 180/math.pi
     return phi_deg_l
 
 def set_angle(angle):
     angle = angle_upper(angle)
+    print(angle)
     pulse_width = int(angle/180*2000 + 500)
-    pi.set_servo_pulsewidth(servo_pin, pulse_width)
+    #pi.set_servo_pulsewidth(servo_pin, pulse_width)
 
 try:
-    angle = int(input("Enter a value between 45 to -45: "))
-    while (angle >= -45 and angle <= 45):
-        angle = angle + 90
-        set_angle(angle)
-        time.sleep(1)
+    angle = int(input("Enter a value between 135 to 45: "))
+    angle = angle - 90
+    set_angle(angle)
+    time.sleep(1)
 
 except KeyboardInterrupt:
     pi.stop()
