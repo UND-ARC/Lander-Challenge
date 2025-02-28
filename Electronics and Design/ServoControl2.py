3711fdd4ecf871a5bc0bb99fb9677a040b957ea7
Welterman <unknown24263@gmail.com>
2025-02-27 18:19:10 -0600

    Updated code to include Christian's servo control

--- Electronics and Design/ServoControl2.py
+++ Electronics and Design/ServoControl2.py
@@ -1,51 +1,37 @@
 import pigpio
+import math
 import time
 
 servo_pin = 18
+r_u = 0.85
+h_u = 4.160360
+r_l = 0.47
+h_l = 2.302372
 
 pi = pigpio.pi()
 pi.set_mode(servo_pin, pigpio.OUTPUT)
 
+def angle_upper(angle):
+    phi_rad_u = math.asin(r_u*math.sin(angle*pi/180)/math.sqrt(r_u**2 + h_u**2 - 2*r_u*h_u*math.cos(angle*pi/180)))
+    phi_deg_u = phi_rad_u * 180/pi
+    return phi_deg_u
+
+def angle_lower(angle):
+    phi_rad_l = math.asin(r_l*math.sin(angle*pi/180)/math.sqrt(r_l**2 + h_l**2 - 2*r_l*h_l*math.cos(angle*pi/180)))
+    phi_deg_l = phi_rad_l * 180/pi
+    return phi_deg_l
+
 def set_angle(angle):
+    angle = angle_upper(angle)
     pulse_width = int(angle/180*2000 + 500)
     pi.set_servo_pulsewidth(servo_pin, pulse_width)
 
 try:
-    set_angle(15)
-    time.sleep(1)
-
-    set_angle(30)
-    time.sleep(1)
-
-    set_angle(45)
-    time.sleep(1)
-
-    set_angle(60)
-    time.sleep(1)
-
-    set_angle(75)
-    time.sleep(1)
-
-    set_angle(90)
-    time.sleep(1)
-
-    set_angle(105)
-    time.sleep(1)
-
-    set_angle(120)
-    time.sleep(1)
-
-    set_angle(135)
-    time.sleep(1)
-
-    set_angle(150)
-    time.sleep(1)
-
-    set_angle(165)
-    time.sleep(1)
-
-    set_angle(180)
-    time.sleep(1)
+    angle = int(input("Enter a value between 45 to -45: "))
+    while (angle >= -45 and angle <= 45):
+        angle = angle + 90
+        set_angle(angle)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     pi.stop()
