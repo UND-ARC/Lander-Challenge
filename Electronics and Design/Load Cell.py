f813f369efd0dd564d6a553c26f0f2e97f333009
Welterman <unknown24263@gmail.com>
2025-03-07 09:43:17 -0600

    Fixed small error

--- Electronics and Design/Load Cell.py
+++ Electronics and Design/Load Cell.py
@@ -5,7 +5,7 @@ from hx711 import HX711
 
 GPIO.setwarnings(False)
 
-startTime = time.perf_counter
+startTime = time.perf_counter()
 
 try:
         hx711 = HX711(
@@ -17,6 +17,6 @@ try:
 
         hx711.reset()   # Before we start, reset the HX711 (not obligate)
         while(True):
-            print("Weight:", hx711.get_raw_data(1), "g", "Time:", time.perf_counter - startTime)
+            print("Weight:", hx711.get_raw_data(1), "g", "Time:", time.perf_counter() - startTime)
 finally:
     GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
