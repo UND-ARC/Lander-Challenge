497e3be9faed2217d6c2864217f02519a5f6bc6d
Welterman <unknown24263@gmail.com>
2025-03-07 11:52:21 -0600

    Switched libraries, slightly more accurate at measuring weight

--- Electronics and Design/Load Cell.py
+++ Electronics and Design/Load Cell.py
@@ -5,27 +5,23 @@ import RPi.GPIO as GPIO
 from hx711 import HX711
 
 GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
 times = []
 weights = []
 
 startTime = time.perf_counter()
 
 try:
-    hx711 = HX711(
-            dout_pin=5,
-            pd_sck_pin=6,
-            channel='A',
-            gain=64
-        )
+    hx711 = HX711(5, 6)
 
-    hx711.reset()   # Before we start, reset the HX711 (not obligate)
-    while(time.perf_counter() - startTime < 40):
-        read = hx711._read()
+    hx711.zero()   # Before we start, reset the HX711 (not obligate)
+    while(time.perf_counter() - startTime < 20):
+        read = hx711.get_data_mean()
         cTime = round(time.perf_counter() - startTime, 2)
         print("Weight:", read, "g", "Time:", cTime)
         times.append(cTime)
         weights.append(read/1000)
-        time.sleep(1)
+        #time.sleep(1)
         
 finally:
     GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
