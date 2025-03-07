1c635fd7c854dd315bc1d125d50a5639c40ec664
BilliamRowe <william.m.rowe@ndus.edu>
2025-03-07 10:41:03 -0600

    Update Load Cell.py

--- Electronics and Design/Load Cell.py
+++ Electronics and Design/Load Cell.py
@@ -1,23 +1,34 @@
 import time
 import sys
+import matplotlib.pyplot as plt
 import RPi.GPIO as GPIO
 from hx711 import HX711
 
 GPIO.setwarnings(False)
+times = []
+weights = []
 
 startTime = time.perf_counter()
 
 try:
-        hx711 = HX711(
+    hx711 = HX711(
             dout_pin=5,
             pd_sck_pin=6,
             channel='A',
             gain=64
         )
 
-        hx711.reset()   # Before we start, reset the HX711 (not obligate)
-        while(True):
-            print("Weight:", hx711._read(), "g", "Time:", round((time.perf_counter() - startTime), 3))
-            time.sleep(1)
+    hx711.reset()   # Before we start, reset the HX711 (not obligate)
+    while(True):
+        print("Weight:", hx711.get_raw_data(1), "g", "Time:", round((time.perf_counter() - startTime), 2))
+        times.append(time.perf_counter() - startTime)
+        weights.append(hx711.get_raw_data()/1000)
+        time.sleep(1)
+        
 finally:
     GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
+    fig1 = plt.figure()
+    plt.plot(times, weights, 'blue')
+    plt.xlabel('Time(seconds)')
+    plt.ylabel('Weights(kg)')
+    plt.title('Weight vs Time')    
