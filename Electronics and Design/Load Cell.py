23e7ed41aec51e82a649b373d682d1ba4ceaf899
Welterman <unknown24263@gmail.com>
2025-03-07 11:12:38 -0600

    Updated to include new library

--- Electronics and Design/Load Cell.py
+++ Electronics and Design/Load Cell.py
@@ -19,16 +19,20 @@ try:
         )
 
     hx711.reset()   # Before we start, reset the HX711 (not obligate)
-    while(True):
-        print("Weight:", hx711.get_raw_data(1), "g", "Time:", round((time.perf_counter() - startTime), 2))
-        times.append(time.perf_counter() - startTime)
-        weights.append(hx711.get_raw_data()/1000)
+    while(time.perf_counter() - startTime < 40):
+        read = hx711._read()
+        cTime = round(time.perf_counter() - startTime, 2)
+        print("Weight:", read, "g", "Time:", cTime)
+        times.append(cTime)
+        weights.append(read/1000)
         time.sleep(1)
         
 finally:
     GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
+    print("done")
     fig1 = plt.figure()
     plt.plot(times, weights, 'blue')
     plt.xlabel('Time(seconds)')
-    plt.ylabel('Weights(kg)')
+    plt.ylabel('Weights(g)')
     plt.title('Weight vs Time')    
+    plt.show()
