b65783a1e56aa979ed0d360d82478236052e1870
Welterman <unknown24263@gmail.com>
2025-03-06 19:48:50 -0600

    Updated with experimental code

--- Electronics and Design/Load Cell.py
+++ Electronics and Design/Load Cell.py
@@ -1,20 +1,39 @@
+import time
+import sys
 import RPi.GPIO as GPIO
 from hx711 import HX711
 
 GPIO.setwarnings(False)
 
-try:
-    hx711 = HX711(
-        dout_pin=5,
-        pd_sck_pin=6,
-        gain=64,
-        channel='A'
-    )
+def cleanAndExit():
+    print("Cleaning...")
+        
+    print("Bye!")
+    sys.exit()
 
-    hx711.reset()
-    measures = hx711.get_raw_data(num_measures=3)
+referenceUnit = 1
+hx = HX711(5, 6)
 
-finally:
-    GPIO.cleanup()
 
-print("\n".join(measures))
+hx.set_reading_format("MSB", "MSB")
+
+#hx.set_reference_unit(113)
+hx.set_reference_unit(referenceUnit)
+
+hx.reset()
+
+hx.tare()
+
+print("Tare done! Add weight now...")
+
+while True:
+    try:
+        val = hx.get_weight(5)
+        print(val)
+
+        hx.power_down()
+        hx.power_up()
+        time.sleep(0.1)
+
+    except (KeyboardInterrupt, SystemExit):
+        cleanAndExit()
