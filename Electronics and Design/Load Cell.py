285cfdf15d4ae8e77f7d641a77112a01b33ffeca
Welterman <unknown24263@gmail.com>
2025-03-07 09:42:28 -0600

    Updated load cell to also measure time

--- Electronics and Design/Load Cell.py
+++ Electronics and Design/Load Cell.py
@@ -5,6 +5,8 @@ from hx711 import HX711
 
 GPIO.setwarnings(False)
 
+startTime = time.perf_counter
+
 try:
         hx711 = HX711(
             dout_pin=5,
@@ -15,6 +17,6 @@ try:
 
         hx711.reset()   # Before we start, reset the HX711 (not obligate)
         while(True):
-            print(hx711.get_raw_data(1), "g")
+            print("Weight:", hx711.get_raw_data(1), "g", "Time:", time.perf_counter - startTime)
 finally:
     GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
