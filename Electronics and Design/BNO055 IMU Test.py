34cb466bd99042b36c5e617468148ecbc9dc5f30
Welterman <unknown24263@gmail.com>
2025-03-09 16:41:53 -0500

    Switched to newer BNO055 library

--- Electronics and Design/BNO055 IMU Test.py
+++ Electronics and Design/BNO055 IMU Test.py
@@ -1,69 +1,67 @@
-import logging
-import sys
 import time
 
-from Adafruit_BNO055 import BNO055
+from adafruit_extended_bus import ExtendedI2C as I2C
+
+import adafruit_bno055
 
 
-# Create and configure the BNO sensor connection.  Make sure only ONE of the
-# below 'bno = ...' lines is uncommented:
-# Raspberry Pi configuration with serial UART and RST connected to GPIO 18:
-bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
-# BeagleBone Black configuration with default I2C connection (SCL=P9_19, SDA=P9_20),
-# and RST connected to pin P9_12:
-#bno = BNO055.BNO055(rst='P9_12')
+# To enable i2c-gpio, add the line `dtoverlay=i2c-gpio` to /boot/config.txt
+
+# Then reboot the pi
 
 
-# Enable verbose debug logging if -v is passed as a parameter.
-if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
-    logging.basicConfig(level=logging.DEBUG)
+# Create library object using our Extended Bus I2C port
 
-# Initialize the BNO055 and stop if something went wrong.
-if not bno.begin():
-    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+# Use `ls /dev/i2c*` to find out what i2c devices are connected
 
-# Print system status and self test result.
-status, self_test, error = bno.get_system_status()
-print('System status: {0}'.format(status))
-print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
-# Print out an error if system status is in error mode.
-if status == 0x01:
-    print('System error: {0}'.format(error))
-    print('See datasheet section 4.3.59 for the meaning.')
+i2c = I2C(1)  # Device is /dev/i2c-1
 
-# Print BNO055 software revision and other diagnostic data.
-sw, bl, accel, mag, gyro = bno.get_revision()
-print('Software version:   {0}'.format(sw))
-print('Bootloader version: {0}'.format(bl))
-print('Accelerometer ID:   0x{0:02X}'.format(accel))
-print('Magnetometer ID:    0x{0:02X}'.format(mag))
-print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
+sensor = adafruit_bno055.BNO055_I2C(i2c)
 
-print('Reading BNO055 data, press Ctrl-C to quit...')
+
+last_val = 0xFFFF
+
+
+
+def temperature():
+
+    global last_val  # pylint: disable=global-statement
+
+    result = sensor.temperature
+
+    if abs(result - last_val) == 128:
+
+        result = sensor.temperature
+
+        if abs(result - last_val) == 128:
+
+            return 0b00111111 & result
+
+    last_val = result
+
+    return result
+
+
+
 while True:
-    # Read the Euler angles for heading, roll, pitch (all in degrees).
-    heading, roll, pitch = bno.read_euler()
-    # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
-    sys, gyro, accel, mag = bno.get_calibration_status()
-    # Print everything out.
-    print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
-          heading, roll, pitch, sys, gyro, accel, mag))
-    # Other values you can optionally read:
-    # Orientation as a quaternion:
-    #x,y,z,w = bno.read_quaterion()
-    # Sensor temperature in degrees Celsius:
-    #temp_c = bno.read_temp()
-    # Magnetometer data (in micro-Teslas):
-    #x,y,z = bno.read_magnetometer()
-    # Gyroscope data (in degrees per second):
-    #x,y,z = bno.read_gyroscope()
-    # Accelerometer data (in meters per second squared):
-    #x,y,z = bno.read_accelerometer()
-    # Linear acceleration data (i.e. acceleration from movement, not gravity--
-    # returned in meters per second squared):
-    #x,y,z = bno.read_linear_acceleration()
-    # Gravity acceleration data (i.e. acceleration just from gravity--returned
-    # in meters per second squared):
-    #x,y,z = bno.read_gravity()
-    # Sleep for a second until the next reading.
+
+    print("Temperature: {} degrees C".format(temperature()))
+
+    print("Accelerometer (m/s^2): {}".format(sensor.acceleration))
+
+    print("Magnetometer (microteslas): {}".format(sensor.magnetic))
+
+    print("Gyroscope (rad/sec): {}".format(sensor.gyro))
+
+    print("Euler angle: {}".format(sensor.euler))
+
+    print("Quaternion: {}".format(sensor.quaternion))
+
+    print("Linear acceleration (m/s^2): {}".format(sensor.linear_acceleration))
+
+    print("Gravity (m/s^2): {}".format(sensor.gravity))
+
+    print()
+
+
     time.sleep(1)
