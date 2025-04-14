4f1c81b43a96119430247fc660531c0f661a33e7
BilliamRowe <william.m.rowe@ndus.edu>
2025-04-14 10:54:15 -0500

    Add files via upload

---
+++ Electronics and Design/Combined Sensors/Lidar_and_Ultrasonic.py
@@ -0,0 +1,73 @@
+import time
+import RPi.GPIO as GPIO
+import smbus
+
+
+I2C_BUS = 1
+I2C_ADDRESS = 0x31
+
+bus = smbus.SMBus(I2C_BUS)
+
+PIN_TRIGGER = 18
+PIN_ECHO = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+GPIO.setup(PIN_ECHO, GPIO.IN)
+
+def near_distance():                                            # Ultrasonic Code
+    GPIO.output(PIN_TRIGGER, False)
+    time.sleep(0.1)
+
+    GPIO.output(PIN_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(PIN_TRIGGER, False)
+
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+    while GPIO.input(PIN_ECHO) == 0:
+        pulse_start_time = time.time()
+    while GPIO.input(PIN_ECHO) == 1:
+        pulse_end_time = time.time()
+
+    pulse_duration = pulse_end_time - pulse_start_time
+    distance = round((pulse_duration * 34300)/2, 3)
+
+    return distance
+
+def far_distance():                                             # LIDAR Code
+    try:
+        data = bus.read_i2c_block_data(I2C_ADDRESS, 0, 2)
+
+        distance = (data[0] << 8 | data[1])
+
+        if (distance == 65535):
+            return None
+        return round((distance / 1000.0), 3)
+
+    except Exception as e:
+        print(f"Error reading from sensor: {e}")
+        return None
+
+'''
+MAIN CODE BEGINS HERE
+'''
+
+distance = 0.0                                                  # Start by reading Ultrasonic
+try:
+    while True:
+        # Measurements Here
+        if (distance > 1):                                      # If over 1 meter, read from LIDAR
+            distance = far_distance()
+        else:
+            distance = near_distance()                          # If distance less than 1 meter, read from Ultrasonic
+        
+        # Converting and printing
+        inch = round(distance / 2.54, 2)
+        feet = round(inch / 12, 2)
+        print("Distance:", distance, "cm", inch, "in", feet, "ft")
+        time.sleep(1)
+
+except KeyboardInterrupt:                                       # Stop code when LCtrl + C is pushed
+    print("Measurement stopped")
+    GPIO.cleanup()
