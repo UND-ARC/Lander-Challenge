f1d23509a26db0422b9d62e55b55f30507493c7e
Gameknight77YT <jacob_schroeder6@proton.me>
2026-02-21 14:47:26 -0600

    added labjack connection, untested

---
+++ Ground Support Controls/Ignitor-Test-GUI-python/LabJackWorker.py
@@ -0,0 +1,101 @@
+import time
+import csv
+from datetime import datetime
+from PyQt6 import QtCore
+from labjack import ljm
+
+class LabJackWorker(QtCore.QObject):
+    # Signal to send the voltage to the UI
+    #voltage_received = QtCore.pyqtSignal(float)
+    CH4Temp_signal = QtCore.pyqtSignal(float)
+    GOXTemp_signal = QtCore.pyqtSignal(float)
+    error_occurred = QtCore.pyqtSignal(str)
+
+    #AIN 0-5 pressures, 12-13 temps
+    channels_to_read = ["AIN0", "AIN1", "AIN2", "AIN3", "AIN4", "AIN5", "AIN12", "AIN13"]
+    num_channels = len(channels_to_read)
+
+    def __init__(self, handle):
+        super().__init__()
+        self.handle = handle
+        self.loggingEnabled = False
+        self._running = True
+        self._output_queue = None  # To store pending writes
+        self.filename = "temp.csv"
+
+    def run(self):
+        try:
+
+            while self._running:
+                # 1. Read from LabJack (This is the "blocking" part)
+                # value = ljm.eReadName(handle, "AIN0")
+                #value = 3.14  # Simulated reading
+                results = ljm.eReadNames(self.handle, self.num_channels, self.channels_to_read)
+
+                #pressures
+                pressure0 = results[0]
+                pressure1 = results[1]
+                pressure2 = results[2]
+                pressure3 = results[3]
+                pressure4 = results[4]
+                pressure5 = results[5]
+
+                #Temps
+                temp12 = results[6]
+                temp13 = results[7]
+
+                CH4Temp = scale_value(temp12, 0, 10, -150, 1370)
+                GOXTemp = scale_value(temp13, 0, 10, -150, 1370)
+
+                if self.loggingEnabled:
+                    self.write_to_csv((CH4Temp, GOXTemp))
+
+                # 2. Emit the signals
+                #self.voltage_received.emit(value)
+                self.CH4Temp_signal.emit(CH4Temp)
+                self.GOXTemp_signal.emit(GOXTemp)
+
+                # 3. Frequency of reading (e.g., 10Hz = 0.1)
+                time.sleep(0.1)
+        except Exception as e:
+            self.error_occurred.emit(str(e))
+
+    @QtCore.pyqtSlot(str, float)
+    def write_value(self, name, value):
+        """This method will be called by the GUI."""
+        try:
+            # Direct write to the LabJack (e.g., setting a DAC or Digital Out)
+            ljm.eWriteName(self.handle, name, value)
+        except Exception as e:
+            print(f"Write Error: {e}")
+
+    def stop(self):
+        self._running = False
+
+    def setLoggingEnabled(self, enabled):
+        if not self.loggingEnabled and enabled:
+            self.loggingEnabled = True
+            self.filename = f"TestLog-{datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")}.csv"
+            self.write_to_csv(("CH4Temp", "GOXTemp")) #headers
+
+
+    def write_to_csv(self, data):
+        # Open in 'append' mode so we don't overwrite previous data
+        with open(self.filename, mode='a', newline='') as f:
+            writer = csv.writer(f)
+            # Create a timestamp
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+            # Write: Timestamp, AIN0, AIN1
+            writer.writerow([timestamp] + list(data))
+
+
+def scale_value(voltage, min_in, max_in, min_out, max_out):
+    # Prevent division by zero if the input range is invalid
+    if max_in == min_in:
+        return min_out
+
+    # Calculate the scaled value
+    scaled = min_out + ((voltage - min_in) * (max_out - min_out) / (max_in - min_in))
+
+    return scaled
+
--- Ground Support Controls/Ignitor-Test-GUI-python/app.py
+++ Ground Support Controls/Ignitor-Test-GUI-python/app.py
@@ -1,8 +1,13 @@
 import sys
 from PyQt6 import QtCore, QtGui, QtWidgets
+
+from LabJackWorker import LabJackWorker
 #from PyQt6 import uic
 from MainWindow import Ui_MainWindow
+from labjack import ljm
 
+
+#run to covert .ui file to python
 #pyuic6 mainwindow.ui -o MainWindow.py
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -12,11 +17,43 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         #uic.loadUi("mainwindow.ui", self)
         self.setupUi(self)
 
+        #start all valves closed
+        self.closeCH4Valve()
+        self.closeGOXValve()
+        self.stopFireSparkPlug()
+        self.closeNitrogenValve()
 
+        #LabJack data
+        self.thread = QtCore.QThread()
+        handle = None
+        try:
+            handle = ljm.openS("T7", "ANY", "ANY")
+        except Exception as e:
+            print(e)
 
-        # Initialize a timer
+        self.worker = LabJackWorker(handle)
+        self.worker.moveToThread(self.thread)
+
+        # Connect signals
+        self.thread.started.connect(self.worker.run)
+        #self.worker.voltage_received.connect(self.update_lcd)
+        self.worker.error_occurred.connect(self.handle_error)
+
+        self.thread.start()
+
+        # Initialize the timers
+        self.testTimerProgressBar = QtCore.QTimer()
+        self.testTimerProgressBar.timeout.connect(self.update_progressBar)
+
         self.testTimer = QtCore.QTimer()
-        self.testTimer.timeout.connect(self.update_progress)
+        self.testTimer.timeout.connect(self.endTest)
+
+
+        self.testTimerCH4Delay = QtCore.QTimer()
+        self.testTimerCH4Delay.timeout.connect(self.CH4DelayTimeout)
+
+        self.testTimerSparkDelay = QtCore.QTimer()
+        self.testTimerSparkDelay.timeout.connect(self.SparkDelayTimeout)
 
         # Track our current position
         self.current_step = 0
@@ -25,11 +62,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # Connect the button
         self.StartTest.clicked.connect(self.start_test)
 
+        self.NitrogenPurge.clicked.connect(self.nitrogenPurgeClicked)
+        self.DataLogging.clicked.connect(self.DataLogging)
+
+
+
     def start_test(self):
+        #progress bar
         # 1. Get duration in seconds from spinbox
         duration = self.FiringDurationSeconds.value()
 
-        if duration == 0:
+        if duration <= 0:
             return
 
         # 2. Setup the progress tracking
@@ -38,26 +81,129 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.current_step = 0
         self.total_steps = duration * 10
 
-        self.TestProgress.setMaximum(self.total_steps)
+        self.TestProgress.setMaximum(int(self.total_steps))
         self.TestProgress.setValue(0)
 
         # 3. Disable the button so they can't spam it
         self.StartTest.setEnabled(False)
 
         # 4. Start the timer (100ms interval)
-        self.testTimer.start(100)
+        self.testTimerProgressBar.start(100)
 
-    def update_progress(self):
+        self.testTimer.start(int(duration * 1000))
+        print("Starting Test!")
+
+        #GOX no delay
+        self.openGOXValve()
+
+        #CH4
+        CH4Delay = self.CH4DelaySeconds.value()
+
+        self.testTimerCH4Delay.start(int(CH4Delay*1000))
+
+        #Spark
+        sparkDelay = self.SparkDelaySeconds.value() + CH4Delay
+
+        self.testTimerSparkDelay.start(int(sparkDelay*1000))
+
+
+
+    def update_progressBar(self):
         self.current_step += 1
         self.TestProgress.setValue(self.current_step)
 
+        timeRemainingSec = (self.total_steps - self.current_step)/10
+        self.TimeRemaining.display(timeRemainingSec)
+
         # Check if we are finished
         if self.current_step >= self.total_steps:
-            self.testTimer.stop()
-            self.StartTest.setEnabled(True)
+            self.TimeRemaining.display(timeRemainingSec)
+            self.testTimerProgressBar.stop()
             #self.TestProgress.setValue(0)
-            print("Test Complete!")
-            
+
+    def CH4DelayTimeout(self):
+        self.testTimerCH4Delay.stop()
+        self.openCH4Valve()
+
+    def SparkDelayTimeout(self):
+        self.testTimerSparkDelay.stop()
+        self.startFireSparkPlug()
+
+    def endTest(self):
+        self.testTimer.stop()
+        print("Ending Test!")
+        self.StartTest.setEnabled(True)
+        self.closeCH4Valve()
+        self.closeGOXValve()
+        self.stopFireSparkPlug()
+
+    def setLoggingEnabled(self, checked):
+        if checked:
+            self.DataLogging.setText("ON")
+            self.worker.setLoggingEnabled(True)
+        else:
+            self.DataLogging.setText("OFF")
+            self.worker.setLoggingEnabled(False)
+
+
+    def openGOXValve(self):
+        print("Opening GOX Valve!")
+        self.GOXValveIndicator.setCurrentWidget(self.GOX_Open)
+        self.GOXValveIndicator.show()
+        #TODO
+
+    def closeGOXValve(self):
+        print("Closing GOX Valve!")
+        self.GOXValveIndicator.setCurrentWidget(self.GOX_Closed)
+        self.GOXValveIndicator.show()
+        #TODO
+
+    def openCH4Valve(self):
+        print("Opening CH4 Valve!")
+        self.CH4ValveIndicator.setCurrentWidget(self.CH4_Open)
+        self.CH4ValveIndicator.show()
+        #TODO
+
+    def closeCH4Valve(self):
+        print("Closing CH4 Valve!")
+        self.CH4ValveIndicator.setCurrentWidget(self.CH4_Closed)
+        self.CH4ValveIndicator.show()
+        #TODO
+
+    def nitrogenPurgeClicked(self, checked):
+        if checked:
+            self.NitrogenPurge.setText("ON")
+            self.openNitrogenValve()
+        else:
+            self.NitrogenPurge.setText("OFF")
+            self.closeNitrogenValve()
+
+    def openNitrogenValve(self):
+        print("Opening Nitrogen Valve!")
+        self.worker.write_value("FIO7", 1)
+
+    def closeNitrogenValve(self):
+        print("Closing Nitrogen Valve!")
+        self.worker.write_value("FIO7", 0)
+
+
+    def startFireSparkPlug(self):
+        print("Start Fire Spark Plug!")
+        self.IgnitorIndicator.setCurrentWidget(self.Ignitor_On)
+        self.IgnitorIndicator.show()
+        #TODO
+
+    def stopFireSparkPlug(self):
+        print("Stopped Fire Spark Plug!")
+        self.IgnitorIndicator.setCurrentWidget(self.Ignitor_Off)
+        self.IgnitorIndicator.show()
+        #TODO
+
+    '''Handle LabJack Error'''
+    def handle_error(self, err_msg):
+        print(f"LabJack Error: {err_msg}")
+
+
 
 
 if __name__ == '__main__':
