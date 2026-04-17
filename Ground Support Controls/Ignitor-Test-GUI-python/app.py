1823ee3861eadbb2e35a363e505293c36ef5be6e
Gameknight77YT <82116806+Gameknight77YT@users.noreply.github.com>
2026-04-17 14:09:02 -0500

    testing

--- Ground Support Controls/Ignitor-Test-GUI-python/app.py
+++ Ground Support Controls/Ignitor-Test-GUI-python/app.py
@@ -23,6 +23,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.worker = LabJackWorker()
         self.worker.moveToThread(self.thread)
+        time.sleep(3)
 
         self.GraphWindow = GraphWindow()
         self.actionGraphWindow.triggered.connect(self.toggle_dashboard)
@@ -241,11 +242,13 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             return #if estop is on we don't want fire
         print("Opening GOX Valve!")
         self.ManGOXValve.setText("OPEN")
+        self.ManGOXValve.setStyleSheet("background-color: #2ecc71; color: black; font-weight: bold;") #green
         self.worker.write_value("FIO3", 1)
 
     def closeGOXValve(self):
         print("Closing GOX Valve!")
         self.ManGOXValve.setText("CLOSE")
+        self.ManGOXValve.setStyleSheet("background-color: red; color: white; border-radius: 5px;")
         self.worker.write_value("FIO3", 0)
 
     def openCH4Valve(self):
@@ -253,45 +256,53 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             return  # if estop is on we don't want fire
         print("Opening CH4 Valve!")
         self.ManCH4Valve.setText("OPEN")
+        self.ManCH4Valve.setStyleSheet("background-color: #2ecc71; color: black; font-weight: bold;") #green
         self.worker.write_value("FIO4", 1)
 
     def closeCH4Valve(self):
         print("Closing CH4 Valve!")
         self.ManCH4Valve.setText("CLOSE")
+        self.ManCH4Valve.setStyleSheet("background-color: red; color: white; border-radius: 5px;")
         self.worker.write_value("FIO4", 0)
 
     def openBothNitrogenValves(self):
         print("Opening Both Nitrogen Valves!")
         self.ManPurgeBoth.setText("OPEN")
+        self.ManPurgeBoth.setStyleSheet("background-color: #2ecc71; color: black; font-weight: bold;") #green
         self.openNitrogenValveGOX()
         self.openNitrogenValveCH4()
 
     def closeBothNitrogenValves(self):
         print("Closing Both Nitrogen Valves!")
         self.ManPurgeBoth.setText("CLOSED")
+        self.ManPurgeBoth.setStyleSheet("background-color: red; color: white; border-radius: 5px;")
         self.closeNitrogenValveGOX()
         self.closeNitrogenValveCH4()
 
     def openNitrogenValveGOX(self):
         print("Opening Nitrogen Valve GOX!")
         self.ManPurgeGOX.setText("OPEN")
+        self.ManPurgeGOX.setStyleSheet("background-color: #2ecc71; color: black; font-weight: bold;")  # green
         self.worker.write_value("FIO1", 1)
 
 
     def closeNitrogenValveGOX(self):
         print("Closing Nitrogen Valve GOX!")
         self.ManPurgeGOX.setText("CLOSE")
+        self.ManPurgeGOX.setStyleSheet("background-color: red; color: white; border-radius: 5px;")
         self.worker.write_value("FIO1", 0)
 
     def openNitrogenValveCH4(self):
         print("Opening Nitrogen Valve Ch4!")
         self.ManPurgeCH4.setText("OPEN")
+        self.ManPurgeCH4.setStyleSheet("background-color: #2ecc71; color: black; font-weight: bold;")  # green
         self.worker.write_value("FIO2", 1)
 
 
     def closeNitrogenValveCH4(self):
         print("Closing Nitrogen Valve CH4!")
         self.ManPurgeCH4.setText("CLOSE")
+        self.ManPurgeCH4.setStyleSheet("background-color: red; color: white; border-radius: 5px;")
         self.worker.write_value("FIO2", 0)
 
 
@@ -300,6 +311,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             return #if estop is on we don't want fire
         print("Start Fire Spark Plug!")
         self.ManSpark.setText("ON")
+        self.ManSpark.setStyleSheet("background-color: #2ecc71; color: black; font-weight: bold;")  # green
         self.worker.write_value("FIO6", 1)
         self.sparkTimer.start(333)
 
@@ -307,6 +319,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def stopFireSparkPlug(self):
         print("Stopped Fire Spark Plug!")
         self.ManSpark.setText("OFF")
+        self.ManSpark.setStyleSheet("background-color: red; color: white; border-radius: 5px;")
 
         self.sparkTimer.stop()
         self.worker.write_value("FIO6", 0)
@@ -319,70 +332,74 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.worker.write_value("FIO5", 1)
 
     def displayLabjackValues(self, values):
-        # Format to 2 decimal place
-        self.PT_00.setText(f'{values["PT-00"]:.2f} psi')
-        self.PT_01.setText(f'{values["PT-01"]:.2f} psi')
-        self.PT_02.setText(f'{values["PT-02"]:.2f} psi')
-        self.PT_03.setText(f'{values["PT-03"]:.2f} psi')
-        self.PT_04.setText(f'{values["PT-04"]:.2f} psi')
-        self.PT_05.setText(f'{values["PT-05"]:.2f} psi')
-        self.PT_06.setText(f'{values["PT-06"]:.2f} psi')
-        self.PT_07.setText(f'{values["PT-07"]:.2f} psi')
-        self.PT_08.setText(f'{values["PT-08"]:.2f} psi')
-        self.PT_09.setText(f'{values["PT-09"]:.2f} psi')
-        self.PT_10.setText(f'{values["PT-10"]:.2f} psi')
-        self.PT_11.setText(f'{values["PT-11"]:.2f} psi')
-        self.PT_12.setText(f'{values["PT-12"]:.2f} psi')
+        try:
+            # Format to 2 decimal place
+            self.PT_00.setText(f'{values["PT-00"]:.2f} psi')
+            self.PT_01.setText(f'{values["PT-01"]:.2f} psi')
+            self.PT_02.setText(f'{values["PT-02"]:.2f} psi')
+            self.PT_03.setText(f'{values["PT-03"]:.2f} psi')
+            self.PT_04.setText(f'{values["PT-04"]:.2f} psi')
+            self.PT_05.setText(f'{values["PT-05"]:.2f} psi')
+            self.PT_06.setText(f'{values["PT-06"]:.2f} psi')
+            self.PT_07.setText(f'{values["PT-07"]:.2f} psi')
+            self.PT_08.setText(f'{values["PT-08"]:.2f} psi')
+            self.PT_09.setText(f'{values["PT-09"]:.2f} psi')
+            self.PT_10.setText(f'{values["PT-10"]:.2f} psi')
+            self.PT_11.setText(f'{values["PT-11"]:.2f} psi')
+            self.PT_12.setText(f'{values["PT-12"]:.2f} psi')
 
-        self.TC_15.setText(f'{values["TC-15"]:.2f} °C')
-        self.TC_16.setText(f'{values["TC-16"]:.2f} °C')
-        self.TC_17.setText(f'{values["TC-17"]:.2f} °C')
-        self.TC_18.setText(f'{values["TC-18"]:.2f} °C')
-        self.TC_19.setText(f'{values["TC-19"]:.2f} °C')
+            self.TC_15.setText(f'{values["TC-15"]:.2f} °C')
+            self.TC_16.setText(f'{values["TC-16"]:.2f} °C')
+            self.TC_17.setText(f'{values["TC-17"]:.2f} °C')
+            self.TC_18.setText(f'{values["TC-18"]:.2f} °C')
+            self.TC_19.setText(f'{values["TC-19"]:.2f} °C')
 
 
 
 
-        GOXMassFlowRate, GOXFlowChoked = calculate_flow(values["PT-01"], values["PT-02"], values["TC-15"], "O2")
+            GOXMassFlowRate, GOXFlowChoked = calculate_flow(values["PT-01"], values["PT-02"], values["TC-15"], "O2")
 
-        self.MdotGOX.setText(f'{GOXMassFlowRate:.2f} km/h')
-        if GOXFlowChoked:
-            self.MdotGOX.setStyleSheet("background-color: blue; color: white;")
-        else:
-            self.MdotGOX.setStyleSheet("background-color: yellow; color: black;")
+            self.MdotGOX.setText(f'{GOXMassFlowRate:.2f} km/h')
+            if GOXFlowChoked:
+                self.MdotGOX.setStyleSheet("background-color: blue; color: white;")
+            else:
+                self.MdotGOX.setStyleSheet("background-color: yellow; color: black;")
 
-        CH4MassFlowRate, CH4FlowChoked = calculate_flow(values["PT-05"], values["PT-06"], values["TC-17"], "CH4")
+            CH4MassFlowRate, CH4FlowChoked = calculate_flow(values["PT-05"], values["PT-06"], values["TC-17"], "CH4")
 
-        self.MdotCH4.setText(f'{CH4MassFlowRate:.2f} km/h')
-        if CH4FlowChoked:
-            self.MdotCH4.setStyleSheet("background-color: blue; color: white;")
-        else:
-            self.MdotCH4.setStyleSheet("background-color: yellow; color: black;")
+            self.MdotCH4.setText(f'{CH4MassFlowRate:.2f} km/h')
+            if CH4FlowChoked:
+                self.MdotCH4.setStyleSheet("background-color: blue; color: white;")
+            else:
+                self.MdotCH4.setStyleSheet("background-color: yellow; color: black;")
 
-        upperAlertPressure = 10000
-        lowerAlertPressure = 10
-        if (values["PT-03"] > upperAlertPressure) or (values["PT-03"] < lowerAlertPressure) or (values["PT-04"] > upperAlertPressure) or (values["PT-04"] < lowerAlertPressure):
+            upperAlertPressure = 10000
+            lowerAlertPressure = 10
+            if (values["PT-03"] > upperAlertPressure) or (values["PT-03"] < lowerAlertPressure) or (values["PT-04"] > upperAlertPressure) or (values["PT-04"] < lowerAlertPressure):
 
-            self.PressureAlarm.setCurrentWidget(self.Alarm)
-            self.PressureAlarm.show()
-        else:
-            self.PressureAlarm.setCurrentWidget(self.Normal)
-            self.PressureAlarm.show()
+                self.PressureAlarm.setCurrentWidget(self.Alarm)
+                self.PressureAlarm.show()
+            else:
+                self.PressureAlarm.setCurrentWidget(self.Normal)
+                self.PressureAlarm.show()
 
-        if self.PressureAlarm.currentWidget() == self.Normal and not (self.ESTOP.isChecked() or self.KillIgnitor.isChecked()):
-            self.TestStatus.setCurrentWidget(self.GO)
-            self.TestStatus.show()
-            self.StartTest.setEnabled(True)
-        else:
-            self.TestStatus.setCurrentWidget(self.NOGO)
-            self.TestStatus.show()
-            self.StartTest.setEnabled(False)
+            if self.PressureAlarm.currentWidget() == self.Normal and not (self.ESTOP.isChecked() or self.KillIgnitor.isChecked()):
+                self.TestStatus.setCurrentWidget(self.GO)
+                self.TestStatus.show()
+                self.StartTest.setEnabled(True)
+            else:
+                self.TestStatus.setCurrentWidget(self.NOGO)
+                self.TestStatus.show()
+                self.StartTest.setEnabled(False)
 
-        #Graphs
-        # Add new value to the right of the deque (automatically pushes old ones out)
-        self.GraphWindow.ChamberPressureGraph.updateGraph(values["PT-05"])
-        self.GraphWindow.GOXMassFlowRateGraph.updateGraph(GOXMassFlowRate)
-        self.GraphWindow.CH4MassFlowRateGraph.updateGraph(CH4MassFlowRate)
+            #Graphs
+            # Add new value to the right of the deque (automatically pushes old ones out)
+            self.GraphWindow.ChamberPressureGraph.updateGraph(values["PT-05"])
+            self.GraphWindow.GOXMassFlowRateGraph.updateGraph(GOXMassFlowRate)
+            self.GraphWindow.CH4MassFlowRateGraph.updateGraph(CH4MassFlowRate)
+
+        except Exception as e:
+            print(f"LabJack Error: {e}")
 
     def toggle_dashboard(self):
         if self.GraphWindow.isVisible():
